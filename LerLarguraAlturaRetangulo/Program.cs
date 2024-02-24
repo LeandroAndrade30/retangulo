dbd41f4a4d8728074a102e4927e3d0a3c1159700
Leandro Andrade <leandrade666@gmail.com>
2024-02-24 14:03:06 -0300

    Versao do Programa Ler Largura Altura Retângulo sem Orientação a Objetos

--- LerLarguraAlturaRetangulo/Program.cs
+++ LerLarguraAlturaRetangulo/Program.cs
@@ -6,7 +6,20 @@ namespace LerLarguraAlturaRetangulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe a largura do Retângulo");
+            double Largura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a altura do Retângulo");
+            double Altura = double.Parse(Console.ReadLine());
+            double Area = (Largura * Altura);
+            Console.WriteLine($"A Area do retângulo é: {Area}");
+            double Perimetro = 2 * (Largura + Altura);
+            Console.WriteLine($"O  Perimetro do retângulo é: {Perimetro}");
+            double Diagonal = Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+            Console.WriteLine($"A  Diagonal do retângulo é: {Diagonal.ToString("F2")}");
+
+
+
+
         }
     }
 }
